118d665554d6a201e308e3e208bea6dcbeab1e87
Sebastien Adam <sebastien1adam@gmail.com>
2024-05-30 19:38:06 -0400

    Added code for lambda function Modified JavaScript to send correct parameters that lambda function expects

--- counterscipt.js
+++ counterscipt.js
@@ -1,8 +1,15 @@
-document.addEventListener("DOMContentLoaded", function() {
-    fetch('YOUR_API_GATEWAY_INVOKE_URL/count')
-        .then(response => response.json())
-        .then(data => {
-            document.getElementById('visitor-counter').textContent = `You are visitor number: ${data.quantity}`;
-        })
-        .catch(error => console.error('Error:', error));
+document.addEventListener("DOMContentLoaded", function() {  
+    fetch('YOUR_API_GATEWAY_INVOKE_URL', {
+        method: 'GET',  
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ stat_name: 'visitor_count' }) 
+    })
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById('visitor-counter').textContent = `You are visitor number: ${data.quantity}`;
+    })
+    .catch(error => console.error('Error:', error));
 });
+
