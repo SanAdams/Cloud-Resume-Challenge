5bbe9528ac4c436fecdadf867330ee5e67a25d2b
Sebastien Adam <sebastien1adam@gmail.com>
2024-05-30 00:50:23 -0400

    Fixed error on resume Added counterscipt

--- counterscipt.js
+++ counterscipt.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     fetch('YOUR_API_GATEWAY_INVOKE_URL/count')
         .then(response => response.json())
         .then(data => {
-            document.getElementById('visitor-counter').textContent = `You are visitor number: ${data.count}`;
+            document.getElementById('visitor-counter').textContent = `You are visitor number: ${data.quantity}`;
         })
         .catch(error => console.error('Error:', error));
 });
