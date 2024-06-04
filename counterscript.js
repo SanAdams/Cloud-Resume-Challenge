70ecaff9c99293d2dedcbd68caa82a62476180a3
SanAdams <sebastien1adam@gmail.com>
2024-06-04 19:55:28 -0400

    Changed http method

--- counterscript.js
+++ counterscript.js
@@ -1,14 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {  
     fetch('https://mhk03kw3wg.execute-api.us-east-1.amazonaws.com/early_dev', {
-        method: 'GET',  
+        method: 'POST',  
         headers: {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({ stat_name: 'visitor_count' }) 
     })
     .then(response => response.json())
-    .then(data => {
-        document.getElementById('visitor-counter').textContent = `You are visitor number: ${data.quantity}`;
+    .then(response => {
+        document.getElementById('visitor-counter').textContent = `You are visitor number: ${response.quantity}`;
     })
     .catch(error => console.error('Error:', error));
 });
