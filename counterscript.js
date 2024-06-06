6fce5d2bbd2d63e3fbb2f611cd063a57ff93ba12
Sebastien Adam <sebastien1adam@gmail.com>
2024-06-06 18:21:30 -0400

    changed counterscript

--- counterscript.js
+++ counterscript.js
@@ -3,9 +3,9 @@ document.addEventListener("DOMContentLoaded", function() {
         method: 'POST',  
         headers: {
             'Content-Type': 'application/json',
-            'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,X-Amz-Security-Token,Authorization,X-Api-Key,X-Requested-With,Accept,Access-Control-Allow-Methods,Access-Control-Allow-Origin,Access-Control-Allow-Headers',
+            'Access-Control-Allow-Headers': '*',
             'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+            'Access-Control-Allow-Methods': '*'
         },
         body: JSON.stringify({ stat_name: 'visitor_count' }) 
     })
